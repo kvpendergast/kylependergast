606c28099ecac08b078c48680160a4329643d80d
Kyle Pendergast <kvpendergast@gmail.com>
2018-06-09 00:20:56 -0400

    Added particles.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Header, Segment, Container, Icon } from 'semantic-ui-react';
+import { Grid, Header, Segment, Container, Icon, Image } from 'semantic-ui-react';
+import Background from './Background.js'
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {height: props.height};
@@ -16,13 +16,19 @@ class App extends Component {
     const {height} = this.state
     return (
       <div style={{height: height, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-          <Segment style={{position: 'relative'}} textAlign='center' basic>
-            <Header size='huge'>Kyle Pendergast</Header>
-            <a style={{color: 'black'}} href="https://www.facebook.com/kyle.pendergast.56"><Icon name='facebook' size='huge'/></a>
-            <a style={{color: 'black'}} href="https://www.linkedin.com/in/kylependergast/"><Icon name='linkedin' size='huge'/></a>
-            <a style={{color: 'black'}} href="https://twitter.com/Kvpendergast"><Icon name='twitter' size='huge'/></a>
-            <a style={{color: 'black'}} href="https://www.instagram.com/kvpendergast/"><Icon name='instagram' size='huge'/></a>
-          </Segment>
+          <Background />
+        <Grid>
+          <Grid.Row centered>
+            <Segment style={{position: 'relative'}} textAlign='center' basic>
+              <h1 style={{color: 'black'}}>Kyle Pendergast</h1>
+              <a style={{color: 'black'}} href="https://www.facebook.com/kyle.pendergast.56"><Icon name='facebook' size='huge'/></a>
+              <a style={{color: 'black'}} href="https://www.linkedin.com/in/kylependergast/"><Icon name='linkedin' size='huge'/></a>
+              <a style={{color: 'black'}} href="https://twitter.com/Kvpendergast"><Icon name='twitter' size='huge'/></a>
+              <a style={{color: 'black'}} href="https://www.instagram.com/kvpendergast/"><Icon name='instagram' size="huge" /></a>
+            </Segment>
+        </Grid.Row>
+
+        </Grid>
       </div>
     );
   }
